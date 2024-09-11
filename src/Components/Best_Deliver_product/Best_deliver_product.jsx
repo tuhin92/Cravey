789265220d3f7c0e6b6a1708891eed0f6a8cc9ad
tuhin92 <tuhinalmamun71m@gmail.com>
2024-09-11 23:32:12 +0600

    add best delivered categories

---
+++ src/Components/Best_Deliver_product/Best_deliver_product.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Best_deliver_product = () => {
+    return (
+        <div className='max-w-7xl mx-auto my-12'>
+            <h1 className='text-2xl md:text-4xl font-extrabold text-center text-[#3d3c3c]' style={{ fontFamily: 'Josefin Sans, sans-serif' }}>Best <span className='text-[#0393b7]'>Delivered</span> Categories</h1>
+        </div>
+    );
+};
+
+export default Best_deliver_product;
--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import Banner from './Banner';
+import Best_deliver_product from '../Best_Deliver_product/Best_deliver_product';
+
 
 const Home = () => {
     return (
         <div>
             <Banner></Banner>
+            <Best_deliver_product></Best_deliver_product>
         </div>
     );
 };
