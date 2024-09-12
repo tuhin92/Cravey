6bcd63e715e1547a2c49f59fd93366d8cbf796cb
tuhin92 <tuhinalmamun71m@gmail.com>
2024-09-12 22:45:24 +0600

    add delivery banner

---
+++ src/Components/Home/DeliveryBanner.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const DeliveryBanner = () => {
+  return (
+    <div>
+      <img
+      className="h-auto md:h-[500px] w-full"
+        src="https://i.ibb.co.com/G3xFGdS/diet-food-Banner-Landscape.png"
+        alt=""
+      />
+    </div>
+  );
+};
+
+export default DeliveryBanner;
--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Banner from './Banner';
 import Best_deliver_product from '../Best_Deliver_product/Best_deliver_product';
+import DeliveryBanner from './DeliveryBanner';
 
 
 const Home = () => {
@@ -8,6 +9,7 @@ const Home = () => {
         <div>
             <Banner></Banner>
             <Best_deliver_product></Best_deliver_product>
+            <DeliveryBanner></DeliveryBanner>
         </div>
     );
 };
