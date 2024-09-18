a5b8f90a84a394459cafecd2748304c7d2739f71
tuhin92 <tuhinalmamun71m@gmail.com>
2024-09-18 11:48:53 +0600

    add discont banner and make it responsive

--- src/Components/Discount_Banner/Discount_banner.jsx
+++ src/Components/Discount_Banner/Discount_banner.jsx
@@ -2,13 +2,24 @@ import React from "react";
 
 const Discount_banner = () => {
   return (
-    <div className="text-black max-w-7xl mx-auto my-12">
-      <div>
-        <div>
+    <div className="text-black max-w-7xl mx-auto py-12">
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+        <img
+          src="https://i.ibb.co.com/djkGzNv/Dark-Green-Pizza-Photo-Food-Instagram-Post.png"
+          alt=""
+          className="w-full h-64 md:h-[500px] rounded-2xl object-cover"
+        />
+
+        <div className="grid grid-rows-2 gap-4">
           <img
-            src="https://i.ibb.co.com/djkGzNv/Dark-Green-Pizza-Photo-Food-Instagram-Post.png"
+            src="https://i.ibb.co.com/Tkpm3t9/photo-2024-09-17-22-35-10.jpg"
             alt=""
-            className="w-[450px] h-[320px]"
+            className="w-full h-48 md:h-[240px] rounded-2xl object-cover"
+          />
+          <img
+            src="https://i.ibb.co.com/30FNrT8/photo-2024-09-17-22-35-42.jpg"
+            alt=""
+            className="w-full h-48 md:h-[240px] rounded-2xl object-cover"
           />
         </div>
       </div>
